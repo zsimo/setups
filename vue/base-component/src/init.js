6d273c18e1e78779d7a7ea777a7f41ef810d0ef6
zsimo <zsimo30@gmail.com>
2017-10-17 23:25:10 +0200

    add alternative way to create a vue component

--- vue/base-component/src/init.js
+++ vue/base-component/src/init.js
@@ -13,12 +13,26 @@ document.write(require("html-loader!./index.html"));
 
 document.addEventListener("DOMContentLoaded", function() {
 
+    // one way (just declare the component)
     Vue.component('child', {
         template: require("html-loader!./template.html")
     });
 
+    // alternative way (store a component in a variable)
+    var CommentComponent = {
+        data: function () {
+            return {
+                commentpost: "a comment"
+            };
+        },
+        template: "#comment-template"
+    };
+
     new Vue({
-        el: "#app"
+        el: "#app",
+        components: {
+            "comment": CommentComponent
+        }
     });
 
 
